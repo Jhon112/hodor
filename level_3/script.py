b5eb491a63bd3dbd752def3cd0cb876bd70b66e3
Jhon Arias <jhonarmont@hotmail.com>
2019-10-21 04:52:26 -0500

    Add pytesseract, PIL and subproccess modules. Added request to captcha url to get the captcha img, then write the response to a png file opened as wb mode so bytes could be written, then open the file with Image, edit the img using imagemagic by prompting to bash using check_output method from subprocces module, then get the text inside img using pytesseract module, create key captcha and send request. As pytesseract may file I send 2000 request

--- level_3/script.py
+++ level_3/script.py
@@ -1,29 +1,68 @@
 #!/usr/bin/python3
 """
 
-Send multiple POST request to http://158.69.76.135/level0.php
+Send multiple POST request to http://158.69.76.135/level3.php
 Project is for learning purposes only and this page was created for that
 
 """
 import requests
+from PIL import Image
+import pytesseract
+from subprocess import check_output
 
-# Data gotten from a request made from web page
+# Main URL
+url = 'http://158.69.76.135/level3.php'
 
-url = 'http://158.69.76.135/level2.php'
+#   url to get the captcha img
+captcha_url = 'http://158.69.76.135/captcha.php'
+
+# Open session
 with requests.Session() as session:
+    #   Create headers. User-Agent to send pettion as windows user,
+    #   and referer to keep the page as referer
     headers = {
         'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)',
-        'Referer': 'http://158.69.76.135/level2.php'
+        'Referer': 'http://158.69.76.135/level3.php'
     }
 
-    request = session.options(url)
+    #   Request to start session
+    request = session.get(url)
+    #   Get cookie setted
     cookie = session.cookies['HoldTheDoor']
+
     payload = {
         'id': 904,
         'holdthedoor': 'Submit',
-        'key': cookie
+        'key': cookie,
     }
 
-    for petition in range(1024):
+    # Iterate 2000 times to avoid errors
+    for petition in range(2000):
+        #   As session already has the cookie, make request to get captcha img
+        captcha_request = session.get(captcha_url)
+
+        #   Create a neew img and save the bytes gotten from request
+        with open("captcha.png", mode="wb") as img_file:
+            img_file.write(captcha_request.content)
+
+        #   Open img with Image.open
+        img = Image.open("captcha.png")
+
+        # Use imagemagic for processing and resampling image
+        check_output(['convert', 'captcha.png', '-colorspace', 'gray',
+                      '-threshold', '50%', '-resize', '150%', '-resample',
+                      '1000', 'captcha.png'])
+
+        # -psm 8 (treat the image as a single word)
+        custom_oem_psm_config = r'--psm 8'
+        #   Get the text inside the img
+        captcha = pytesseract.image_to_string(
+            img, config=custom_oem_psm_config, lang="eng")
+
+        #   Create captcha key on payload with text read from img
+        payload['captcha'] = captcha
+
+        #   Post payload obj
         response = session.post(url, data=payload, headers=headers)
+        #   as new cookie is set we can get it and set it as a new key
         payload['key'] = session.cookies['HoldTheDoor']
